a33976ff946b776024b56e50071dd03643950015
victorhermon <victorsilva.silva6@gmail.com>
2022-01-22 16:47:29 -0300

    Nomeclatura utilizadas em metodos que fazem testes

--- leilao/test_dominio.py
+++ leilao/test_dominio.py
@@ -3,7 +3,7 @@ from leilao.dominio import Usuario, Lance, Leilao, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_cresente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -14,6 +14,27 @@ class TestAvaliador(TestCase):
 
         leilao.lances.append(lance_do_yuri)
         leilao.lances.append(lance_do_gui)
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decresente(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        lance_do_yuri = Lance(gui, 100.0)
+        lance_do_gui = Lance(yuri, 150.0)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_do_gui)
+        leilao.lances.append(lance_do_yuri)
 
         avaliador = Avaliador()
         avaliador.avalia(leilao)
